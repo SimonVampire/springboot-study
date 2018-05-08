e87e03517166ac25da25273e112223b678e858fc
zhangxue <zhangxue@zhangxuedeMacBook-Pro.local>
2018-05-08 16:24:37 +0800

    controller 中调用也oK

--- src/main/java/cn/sevendream/springbootstudy/HelloController.java
+++ src/main/java/cn/sevendream/springbootstudy/HelloController.java
@@ -1,12 +1,24 @@
 package cn.sevendream.springbootstudy;
 
+import cn.sevendream.springbootstudy.dao.UserLoginMapper;
+import cn.sevendream.springbootstudy.model.UserLogin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private UserLoginMapper userLoginMapper;
     @GetMapping(value = "/")
     public String sayHello() {
-        return "hello";
+        try{
+            UserLogin userLogin = userLoginMapper.selectByPrimaryKey(11);
+            return userLogin.getUsername();
+        }catch (Exception e){
+            e.printStackTrace();
+            return "出错了 /(ㄒoㄒ)/~~";
+        }
+
     }
 
 }
